c69b79c0ed44be776ced5f58c168905c64a9ec98
luisAngelvv13 <87919225+luisAngelvv13@users.noreply.github.com>
2021-07-28 00:42:13 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,13 @@
+ /*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+      apiKey: "AIzaSyAMErvZsraeSLACx1bxuP8XwLheR4htBHs",
+    authDomain: "autentifi41-15bfb.firebaseapp.com",
+    projectId: "autentifi41-15bfb",
+    storageBucket: "autentifi41-15bfb.appspot.com",
+    messagingSenderId: "311256078646",
+    appId: "1:311256078646:web:d4b51298233947210e7b97",
+    measurementId: "G-JFZ15NFDQQ"
+
+});
